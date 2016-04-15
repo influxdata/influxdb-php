ee814a0914cb19aaf695d9bcbae27c53b635f3eb
Mark Johnson <mark@sunray.net>
2016-04-15 10:47:50 -0700

    Updating Influx Database with support for writing direct payloads which results in over 5x performance increase when writing large Points/Payloads. Docblock updates for improved thoroughness. Cleaning up some extra spaces in Point.

--- src/InfluxDB/Database.php
+++ src/InfluxDB/Database.php
@@ -119,12 +119,15 @@ class Database
     }
 
     /**
-     * Writes points into InfluxDB
+     * Write points into InfluxDB using the current driver. This is the recommended method for inserting
+     * data into InfluxDB.
      *
      * @param  Point[]     $points           Array of Point objects
-     * @param  string      $precision        The timestamp precision (defaults to nanoseconds)
+     * @param  string      $precision        The timestamp precision (defaults to nanoseconds).
      * @param  string|null $retentionPolicy  Specifies an explicit retention policy to use when writing all points. If
-     *                                       not set, the default retention period will be used.
+     *                                       not set, the default retention period will be used. This is only
+     *                                       applicable for the Guzzle driver. The UDP driver utilizes the endpoint
+     *                                       configuration defined in the server's influxdb configuration file.
      * @return bool
      * @throws \InfluxDB\Exception
      */
@@ -137,6 +140,28 @@ class Database
             $points
         );
 
+        return $this->writePayload($payload, $precision, $retentionPolicy);
+    }
+
+    /**
+     * Write a payload into InfluxDB using the current driver. This method is similar to <tt>writePoints()</tt>,
+     * except it takes a string payload instead of an array of Points. This is useful in the following situations:
+     *
+     *   1) Performing unique queries that may not conform to the current Point standard.
+     *   2) Inserting very large set of points into a measurement where looping via array_map() actually
+     *      hurts performance as the payload may be calculated in advance by caller.
+     *
+     * @param  string      $payload          InfluxDB payload that conforms to the Line syntax.
+     * @param  string      $precision        The timestamp precision (defaults to nanoseconds).
+     * @param  string|null $retentionPolicy  Specifies an explicit retention policy to use when writing all points. If
+     *                                       not set, the default retention period will be used. This is only
+     *                                       applicable for the Guzzle driver. The UDP driver utilizes the endpoint
+     *                                       configuration defined in the server's influxdb configuration file.
+     * @return bool
+     * @throws \InfluxDB\Exception
+     */
+    public function writePayload($payload, $precision = self::PRECISION_NANOSECONDS, $retentionPolicy = null)
+    {
         try {
             $parameters = [
                 'url' => sprintf('write?db=%s&precision=%s', $this->name, $precision),
